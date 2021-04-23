d93011e00323001d4befa4d13496b06f12c6e250
dkisloff <dkisloff85@gmail.com>
2021-04-23 14:18:54 +0300

    fix arrow

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/left.jpg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/right.jpg"></button>',
         responsive: [
             {
                 breakpoint: 768,
